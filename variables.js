ece7c6ff85013f66a818f6faf6c6c8ab663c7fc1
Parvin Sattorova <nanajon66@gmail.com>
2022-12-04 17:25:11 -0500

    Add var that are assigned to Numbers

--- variables.js
+++ variables.js
@@ -1,9 +1,13 @@
 // variables that are assigned to Strings
-var email = "helloworld@gmail.com"
+var email = "nanajon66@gmail.com"
 var zipCode = "32550"
 var phoneNumbers = "303-550-8945"
 
-// var startingAge = 37
+// variables that are assigned to Numbers
+var startingAge = 56
+var birthDate = 03
+var birthYear = 1966
+var birthMonth = 04
 
 
 // var depositPaid = true
