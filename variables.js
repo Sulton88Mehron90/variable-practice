ffdf326e2d5a1ae255c08e6eaca6dea489c9cb49
Parvin Sattorova <nanajon66@gmail.com>
2022-12-04 18:51:08 -0500

    Add console.log statements

--- variables.js
+++ variables.js
@@ -1,7 +1,14 @@
 // variables that are assigned to Strings
 var email = "nanajon66@gmail.com"
+email = "lacy7.edwards@gmail.com"
 var zipCode = "32550"
+zipCode = "80228"
 var phoneNumbers = "303-550-8945"
+phoneNumbers = "917-495-7222"
+
+console.log (email)
+console.log (zipCode)
+console.log (phoneNumbers)
 
 // variables that are assigned to Numbers
 var startingAge = 56
@@ -9,6 +16,16 @@ var birthDate = 03
 var birthYear = 1966
 var birthMonth = 04
 
+console.log (startingAge)
+console.log (birthDate)
+console.log (birthYear)
+console.log (birthMonth)
+
 // variable that are assigned to Booleans
 var depositPaid = true
+depositPaid // => true
 var married = false
+married// => false
+
+console.log (depositPaid)
+console.log (married)
