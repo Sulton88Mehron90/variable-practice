dc34ddc60586625754f2a8f76f18db43cea182f3
Parvin Sattorova <nanajon66@gmail.com>
2022-12-04 17:18:13 -0500

    Addvar that assigned to string

---
+++ variables.js
@@ -0,0 +1,9 @@
+// variables that are assigned to Strings
+var email = "helloworld@gmail.com"
+var zipCode = "32550"
+var phoneNumbers = "303-550-8945"
+
+// var startingAge = 37
+
+
+// var depositPaid = true
