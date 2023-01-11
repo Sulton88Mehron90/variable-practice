56ea8dc46302a784e258bca55db12b74e280e81a
Parvin Sattorova <nanajon66@gmail.com>
2023-01-10 19:48:53 -0700

    Add edits to variables that are assigned to Strings

--- variables.js
+++ variables.js
@@ -1,16 +1,23 @@
 // variables that are assigned to Strings
-var email = "nanajon66@gmail.com"
-email = "lacy7.edwards@gmail.com"
+
+var email = "lacy7.edwards@gmail.com"
 var zipCode = "32550"
-zipCode = "80228"
+var phoneNumbers = "917-495-7222"
+
+console.log (email)
+console.log (zipCode)
+console.log (phoneNumbers)
+
+var email = "nanajon66@gmail.com"
+var zipCode = "80228"
 var phoneNumbers = "303-550-8945"
-phoneNumbers = "917-495-7222"
 
 console.log (email)
 console.log (zipCode)
 console.log (phoneNumbers)
 
 // variables that are assigned to Numbers
+
 var startingAge = 56
 var birthDate = 03
 var birthYear = 1966
@@ -22,6 +29,7 @@ console.log (birthYear)
 console.log (birthMonth)
 
 // variable that are assigned to Booleans
+
 var depositPaid = true
 depositPaid // => true
 var married = false
